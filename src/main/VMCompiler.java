f9762e53e1cf4705186e36f598f55ae017280f33
iain.rex <iain.rex@ROPTO-MBP012.broadband>
2021-01-17 19:33:11 +0000

    feat(tests): Add pong test

--- src/main/VMCompiler.java
+++ src/main/VMCompiler.java
@@ -5,7 +5,7 @@ class VMCompiler {
 
   public static void main(String[] args) {
 
-    String dir = "../../test-files/Compiler/Average";
+    String dir = "../../test-files/Compiler/Pong";
 
     LoadFiles files = new LoadFiles(dir, "jack");
 
