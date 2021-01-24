da4bae012f21e646d8ced555096127348087394e
iain.rex <iain.rex@ROPTO-MBP012.broadband>
2021-01-24 13:29:56 +0000

    feat(code-cleanup): Remove compilation engines

--- src/main/compilationEngine/symboltable/SymbolKind.java
+++
@@ -1,6 +0,0 @@
-package compilationEngine.symboltable;
-
-public enum SymbolKind {
-  // @todo: remove null
-  STATIC, FIELD, ARGUMENT, VAR, NULL,
-}
--- src/main/compilationenginexml/symboltable/SymbolKind.java
+++
@@ -1,6 +0,0 @@
-package compilationenginexml.symboltable;
-
-public enum SymbolKind {
-  // @todo: remove null
-  STATIC, FIELD, ARGUMENT, VAR, NULL,
-}
