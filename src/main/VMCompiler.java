645a4961db12a228a182817ccb99beb80c3df044
iain.rex <iain.rex@ROPTO-MBP012.broadband>
2021-01-25 21:52:47 +0000

    feat(code-cleanup): Organize test files

--- src/main/VMCompiler.java
+++ src/main/VMCompiler.java
@@ -5,7 +5,7 @@ class VMCompiler {
 
   public static void main(String[] args) {
 
-    String dir = "../../test-files";
+    String dir = "../../tests";
 
     LoadFiles files = new LoadFiles(dir, "jack", true);
 
